231936b6e1b9bcb33405efb2a4e89ebe4539831c
Fernando Aires Castello <fernandoairescastello@gmail.com>
2023-07-01 16:07:40 -0300

    View history

---
+++ Tools/PTMStudio/v1.1/TileGameMaker/PTMLExporters/ProjectExporter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TileGameLib.File;
+using TileGameLib.GameElements;
+using TileGameLib.Util;
+using TileGameMaker.MapEditorElements;
+
+namespace TileGameMaker.PTMLExporters
+{
+    public class ProjectExporter
+    {
+        public static void ExportEverything(Project proj)
+        {
+            string folder = proj.Folder;
+            string chrFile = Path.Combine(folder, proj.Name + ".chr.dat");
+            string palFile = Path.Combine(folder, proj.Name + ".pal.dat");
+
+            TilesetFile.Export(TilesetExportFormat.BinaryStrings, proj.Tileset, chrFile);
+            PaletteFile.Export(PaletteExportFormat.HexadecimalRgb, proj.Palette, palFile);
+
+            foreach (ObjectMap tilebuf in proj.Maps)
+            {
+                string bufFile = Path.Combine(folder, tilebuf.Id + ".buf.dat");
+                TilebufferExporter.Export(tilebuf, bufFile);
+            }
+        }
+    }
+}
